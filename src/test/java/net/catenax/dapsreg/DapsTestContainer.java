d6eba2bdc3c0779ab74ac81c70e29aaf64a86108
Dmitrii <dmitrii.vasiunin@t-systems.com>
2022-08-15 18:04:10 +0300

    move image name for DAPS test container to the application.yml

--- src/test/java/net/catenax/dapsreg/DapsTestContainer.java
+++ src/test/java/net/catenax/dapsreg/DapsTestContainer.java
@@ -3,6 +3,7 @@ package net.catenax.dapsreg;
 import lombok.RequiredArgsConstructor;
 import net.catenax.dapsreg.service.DapsClient;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
@@ -14,11 +15,14 @@ import javax.annotation.PostConstruct;
 public class DapsTestContainer implements DisposableBean {
 
     private final DapsClient dapsClient;
+
+    @Value("${app.daps.imageName}")
+    private String imageName;
     private GenericContainer<?> myDapsServer;
 
     @PostConstruct
     private void init() {
-        myDapsServer = new GenericContainer<>("my-omejdn-server:latest")
+        myDapsServer = new GenericContainer<>(imageName)
                 .withClasspathResourceMapping("omejdn-config", "/opt/config", BindMode.READ_WRITE)
                 .withClasspathResourceMapping("omejdn-keys", "/opt/keys", BindMode.READ_WRITE)
                 .withEnv("OMEJDN_PLUGINS", "/opt/config/plugins.yml")
